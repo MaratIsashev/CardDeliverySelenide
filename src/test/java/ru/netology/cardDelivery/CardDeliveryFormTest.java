fc8fe291b0d434b14730a4770b80bc174886a74f
Marat Isashev <isashevmarat@gmail.com>
2023-08-24 08:17:37 +0300

    commit

--- src/test/java/ru/netology/cardDelivery/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/cardDelivery/CardDeliveryFormTest.java
@@ -1,12 +1,29 @@
 package ru.netology.cardDelivery;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryFormTest {
 
     @Test
     void testPositive(){
-        open();
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("span[data-test-id=city] input").setValue("Новосибирск");
+        //$("span[data-test-id=date] input").clear();
+        //$("span[data-test-id=date] input").setValue("");
+        $("span[data-test-id=name] input").setValue("Исашев Марат");
+        $("span[data-test-id=phone] input").setValue("+79137940954");
+        $("label[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $(withText("Успешно!")).should(Condition.appear, Duration.ofSeconds(15));
 
     }
 
