5870f5eec04969a6d3e865ad93c20f7ba8a0aaf6
MaratIsashev <116102757+MaratIsashev@users.noreply.github.com>
2023-08-25 10:35:52 +0300

    Update CardDeliveryFormTest.java

--- src/test/java/ru/netology/cardDelivery/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/cardDelivery/CardDeliveryFormTest.java
@@ -5,20 +5,29 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class CardDeliveryFormTest {
 
     @Test
     void testPositive(){
+        Date currentDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.DATE, 4);
+        Date deliveryDate = calendar.getTime();
+        
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("span[data-test-id=city] input").setValue("Новосибирск");
         //$("span[data-test-id=date] input").clear();
-        $("span[data-test-id=date] input").setValue("орп");
+        $("span[data-test-id=date] input").setValue(String.valueOf(deliveryDate));
         $("span[data-test-id=name] input").setValue("Исашев Марат");
         $("span[data-test-id=phone] input").setValue("+79137940954");
         $("label[data-test-id=agreement]").click();
